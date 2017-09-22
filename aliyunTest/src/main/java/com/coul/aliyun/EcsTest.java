d0286b8adec03ba9e20e6911c3259ab4060790ef
TomDtj <dongtangjin@gmail.com>
2017-09-22 08:37:21 +0800

    PropertiesUtils类专门处理Properties配置文件取值

--- aliyunTest/src/main/java/com/coul/aliyun/EcsTest.java
+++ aliyunTest/src/main/java/com/coul/aliyun/EcsTest.java
@@ -1,5 +1,10 @@
 package com.coul.aliyun;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.ecs.model.v20140526.CreateInstanceRequest;
@@ -16,26 +21,31 @@ import com.aliyuncs.profile.DefaultProfile;
 
 public class EcsTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+//		System.out.println(PropertiesUtils.getPropertiesByKey("Format"));
+		startInstance();
 		stopInstance();
 	}
 
-	private static void createInstanceResponse() {
-		// 初始化
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou",
-				"LTAIwtHOAIIRbgWJ", "qH4pNNH5yYatzMgcxKQBCGJu5lei6e");
+	private static IAcsClient getClient(){
+		String accessKeyId = PropertiesUtils.getPropertiesByKey("AccessKeyId");
+		String accessSecret = PropertiesUtils.getPropertiesByKey("Access_Key_Secret");
+		String region = PropertiesUtils.getPropertiesByKey("Region");
+		if(region==null)region = "cn-hangzhou";//Ĭ��ֵcn-hangzhou
+		DefaultProfile profile = DefaultProfile.getProfile(region,
+				accessKeyId, accessSecret);
 		IAcsClient client = new DefaultAcsClient(profile);
-
-		// 设置参数
+		return client;
+	}
+	private static void createInstanceResponse() {
 		CreateInstanceRequest createInstance = new CreateInstanceRequest();
 		createInstance.setInstanceType("ecs.n4.small");
 		createInstance.setImageId("centos_7_02_64_20G_alibase_20170818.vhd");
 		createInstance.setRegionId("cn-shanghai");
 		createInstance.setInstanceChargeType("PostPaid");
 
-		// 发起请求
 		try {
-			CreateInstanceResponse response = client
+			CreateInstanceResponse response = getClient()
 					.getAcsResponse(createInstance);
 			System.out.println(response.getRequestId());
 		} catch (Exception e) {
@@ -44,18 +54,11 @@ public class EcsTest {
 	}
 
 	private static void deleteInstance() {
-		// 初始化
-		DefaultProfile profile = DefaultProfile.getProfile("cn-shanghai",
-				"LTAIwtHOAIIRbgWJ", "qH4pNNH5yYatzMgcxKQBCGJu5lei6e");
-		IAcsClient client = new DefaultAcsClient(profile);
-
-		// 设置参数
 		DeleteInstanceRequest deleteInstance = new DeleteInstanceRequest();
 		deleteInstance.setInstanceId("i-uf633n67cx27b12kytym");
 
-		// 发起请求
 		try {
-			DeleteInstanceResponse response = client
+			DeleteInstanceResponse response = getClient()
 					.getAcsResponse(deleteInstance);
 			System.out.println(response.getRequestId());
 		} catch (Exception e) {
@@ -64,18 +67,11 @@ public class EcsTest {
 	}
 
 	private static void startInstance() {
-		// 初始化
-		DefaultProfile profile = DefaultProfile.getProfile("cn-shanghai",
-				"LTAIwtHOAIIRbgWJ", "qH4pNNH5yYatzMgcxKQBCGJu5lei6e");
-		IAcsClient client = new DefaultAcsClient(profile);
-
-		// 设置参数
 		StartInstanceRequest startInstance = new StartInstanceRequest();
 		startInstance.setInstanceId("i-uf633n67cx27b12kytym");
 
-		// 发起请求
 		try {
-			StartInstanceResponse response = client
+			StartInstanceResponse response = getClient()
 					.getAcsResponse(startInstance);
 			System.out.println(response.getRequestId());
 		} catch (Exception e) {
@@ -85,42 +81,31 @@ public class EcsTest {
 	}
 
 	private static void stopInstance() {
-		// 初始化
-		DefaultProfile profile = DefaultProfile.getProfile("cn-shanghai",
-				"LTAIwtHOAIIRbgWJ", "qH4pNNH5yYatzMgcxKQBCGJu5lei6e");
-		IAcsClient client = new DefaultAcsClient(profile);
-
-		// 设置参数
 		StopInstanceRequest stopInstance = new StopInstanceRequest();
 		stopInstance.setInstanceId("i-uf633n67cx27b12kytym");
 
-		// 发起请求
 		try {
-			StopInstanceResponse response = client.getAcsResponse(stopInstance);
-			
-			System.out.println("RequestId:"+response.getRequestId());
+			StopInstanceResponse response = getClient().getAcsResponse(stopInstance);
+
+			System.out.println("RequestId:" + response.getRequestId());
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
 	}
-	
-	private static void describeInstanceTypes(){
-		// 初始化
-	    DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "<accessKeyId>","<accessSecret>");
-	    IAcsClient client = new DefaultAcsClient(profile);
-	  
-	    //设置参数
-	    DescribeInstanceTypesRequest describeInstanceTypes = new DescribeInstanceTypesRequest();
-	    describeInstanceTypes.setInstanceTypeFamily("ecs.n4");
-	  
-	    // 发起请求
-	    try {
-	      DescribeInstanceTypesResponse response = client.getAcsResponse(describeInstanceTypes);
-	    }catch (Exception e) {
-	      e.printStackTrace();
-	    }
+
+	private static void describeInstanceTypes() {
+
+
+		DescribeInstanceTypesRequest describeInstanceTypes = new DescribeInstanceTypesRequest();
+		describeInstanceTypes.setInstanceTypeFamily("ecs.n4");
+
+		try {
+			DescribeInstanceTypesResponse response = getClient()
+					.getAcsResponse(describeInstanceTypes);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
 
 }
