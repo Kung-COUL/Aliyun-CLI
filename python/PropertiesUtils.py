be47b6958211b89f975dd746e40068c190603e00
TomDtj <dongtangjin@gmail.com>
2017-09-30 16:32:00 +0800

    fix conflict

--- python/PropertiesUtils.py
+++ python/PropertiesUtils.py
@@ -25,3 +25,30 @@ with open('properties.json') as json_data:
 
     def diskId():
         return data['DiskId']
+import json
+with open('properties.json') as json_data:
+    data = json.load(json_data)
+
+    def keyPairName():
+        return data['KeyPairName']
+
+    def accessKeyId():
+        return data['AccessKeyId']
+
+    def accessSecret():
+        return data['AccessSecret']
+
+    def regionId():
+        return data['RegionId']
+
+    def outputFormat():
+        return data['OutputFormat']
+
+    def instanceId():
+        return data['InstanceId']
+
+    def allocationId():
+        return data['AllocationId']
+
+    def diskId():
+        return data['DiskId']
