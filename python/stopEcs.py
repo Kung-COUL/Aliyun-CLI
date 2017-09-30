ff15c2d3f333088faba0f15746331076ffda2e35
TomDtj <dongtangjin@gmail.com>
2017-09-30 20:10:55 +0800

    fix some bugs

--- python/stopEcs.py
+++ python/stopEcs.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 #coding=utf-8
-#pip install aliyun-python-sdk-core --target=.
-#pip install aliyun-python-sdk-ecs --target=.
 
 from lib.aliyunsdkcore import client
 from lib.aliyunsdkecs.request.v20140526 import StopInstanceRequest
 import PropertiesUtils as p
 
-
 def doIt():
     clt = client.AcsClient(p.accessKeyId(), p.accessSecret(), p.regionId())
+#    print type(p.accessKeyId())
+#    print type( p.accessSecret())
+#    print type(p.regionId())
 
     # 设置参数
     request = StopInstanceRequest.StopInstanceRequest()
     request.set_accept_format('json')
+#    print type(p.instanceId())
 
     request.add_query_param('InstanceId', p.instanceId())
 
+#    print type(request)
     # 发起请求
     response = clt.do_action_with_exception(request)
-
-# 输出结果
-if __name__ == "__main__":
-    print response
+    #输出结果
+    if __name__ == "__main__":
+        print response
