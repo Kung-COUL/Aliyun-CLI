81391b39cb23481dcddc755d49e09a874598c813
Kung-COUL <kung@coul.com>
2017-10-01 17:31:13 +0800

    add describeImages.py

---
+++ python/describeImages.py
@@ -0,0 +1,29 @@
+
+# coding=utf-8
+#!/usr/bin/env python
+
+from lib.aliyunsdkcore import client
+from lib.aliyunsdkecs.request.v20140526 import DescribeImagesRequest
+import PropertiesUtils as p
+import json
+
+clt = client.AcsClient(p.accessKeyId(), p.accessSecret(), p.regionId())
+
+# 设置参数
+request = DescribeImagesRequest.DescribeImagesRequest()
+request.set_accept_format('json')
+
+# 发起请求
+response = clt.do_action(request)
+
+# 输出结果
+imageDict = json.loads(response)['Images']['Image']
+totalCount = len(imageDict)
+
+print totalCount
+print
+
+for i in range(totalCount):
+    print json.dumps(imageDict[i]['ImageId'], indent=2)
+
+print
